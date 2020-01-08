33de9e0efde3ba62b0ba6962b975d326ca951ab0
ahmadAtrach <54948072+ahmadAtrach@users.noreply.github.com>
2020-01-08 17:08:11 +0200

    Initial commit

---
+++ app/src/main/java/com/akkar/akar/registerActivity.java
@@ -0,0 +1,124 @@
+package com.akkar.akar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class registerActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText editTextName, editTextEmail, editTextReapEmail, editTextPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        getSupportActionBar().hide();
+        editTextEmail = (EditText) findViewById(R.id.mail);
+        editTextName = (EditText) findViewById(R.id.username);
+        editTextReapEmail = (EditText) findViewById(R.id.repeated_mail);
+        editTextPassword = (EditText) findViewById(R.id.password);
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.button_register).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            //handle the already login user
+        }
+    }
+
+    private void registerUser() {
+        final String name = editTextName.getText().toString().trim();
+        final String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        final String reapmMail = editTextReapEmail.getText().toString().trim();
+        if (name.isEmpty()) {
+            editTextName.setError(getString(R.string.input_error_name));
+            editTextName.requestFocus();
+            return;
+        }
+        if (email.isEmpty()) {
+            editTextEmail.setError(getString(R.string.input_error_email));
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError(getString(R.string.input_error_email_invalid));
+            editTextEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            editTextPassword.setError(getString(R.string.input_error_password));
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (password.length() < 8) {
+            editTextPassword.setError(getString(R.string.input_error_password_length));
+            editTextPassword.requestFocus();
+            return;
+        }
+        if (reapmMail.compareTo(email) != 0) {
+            editTextReapEmail.setError(getString(R.string.input_error_email_didnot_match));
+            editTextEmail.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                    User user = new User(
+                            name,
+                            email
+                    );
+                    FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                            .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            progressBar.setVisibility(View.GONE);
+                            if (task.isSuccessful()) {
+                                Toast.makeText(registerActivity.this, getString(R.string.registration_success), Toast.LENGTH_LONG).show();
+                            } else {
+                                //display a failure message
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(registerActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+
+        public void onClick (View v){
+            switch (v.getId()) {
+                case R.id.button_register:
+                    registerUser();
+                    break;
+            }
+        }
+}
+
+
+
