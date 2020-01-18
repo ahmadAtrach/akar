5d47aa47dcf5532e344b69cfa5e236a3deae2fed
ahmadAtrach <54948072+ahmadAtrach@users.noreply.github.com>
2020-01-18 20:31:23 +0200

    add login xml page

--- app/src/main/java/com/akkar/akar/loginActivity.java
+++ app/src/main/java/com/akkar/akar/loginActivity.java
@@ -3,13 +3,17 @@ package com.akkar.akar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class loginActivity extends AppCompatActivity {
-
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        progressBar = findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
     }
 }
