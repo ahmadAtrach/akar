9631ae873a01d55852bcaabdfe94826d8509bdca
ahmadAtrach <54948072+ahmadAtrach@users.noreply.github.com>
2020-01-19 18:36:06 +0200

    add: 3 fragments and link it to homepage activity

--- app/src/main/java/com/akkar/akar/homePageActivity.java
+++ app/src/main/java/com/akkar/akar/homePageActivity.java
@@ -1,15 +1,72 @@
 package com.akkar.akar;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
-public class homePageActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+public class homePageActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener,
+        homeFragment.OnFragmentInteractionListener,searchFragment.OnFragmentInteractionListener,
+        profileFragment.OnFragmentInteractionListener
+
+{
+    Fragment homePage = new homeFragment();
+    TextView title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         getSupportActionBar().hide();
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        title = (TextView) findViewById(R.id.title);
+        title.setText(getResources().getString(R.string.title_home));
+        loadFragment(homePage);
+
+    }
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment;
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                title.setText(getResources().getString(R.string.title_home));
+                loadFragment(homePage);
+                return true;
+            case R.id.navigation_profile:
+                title.setText(getResources().getString(R.string.title_profile));
+                loadFragment(new profileFragment());
+                return true;
+            case R.id.navigation_search:
+                title.setText(getResources().getString(R.string.title_search));
+                loadFragment(new searchFragment());
+                return true;
+        }
+        return false;
+    }
+        public void loadFragment(Fragment fragment) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frameContainer, fragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+    public void onContentFragmentInteraction(String string) {
+        // Do different stuff
+    }
+
+
+    @Override
+    public void OnFragmentInteractionListener(String string) {
+
+    }
+
+    @Override
+    public void onContentFragmentInteraction2(String string) {
+
     }
 }
